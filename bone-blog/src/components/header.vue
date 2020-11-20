a144773a3ff911b07f1dd90bd1a138f49b309a4d
lzc <you@example.com>
2020-11-20 18:09:26 +0800

    1120

---
+++ bone-blog/src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="header">
+		<p>bone的博客</p>
+		<div class="header-nav">
+			<span>前端文章</span>
+			<span>前端文章</span>
+			<span>前端文章</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data:function () {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	
+</style>
