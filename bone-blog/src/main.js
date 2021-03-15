5740ef268ad00e95369bb66f97b7779da08bf208
lzc <you@example.com>
2021-03-15 13:21:49 +0800

    315

--- bone-blog/src/main.js
+++ bone-blog/src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'weakmap-polyfill';
+import WeakMap from 'weakmap-polyfill';
 
+window.WeakMap = WeakMap;
 Vue.config.productionTip = false
 
 new Vue({
